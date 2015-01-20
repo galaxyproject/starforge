1f130f4b8b457e1be58f0c8c179bd5b2a463b585
Nate Coraor <nate@bx.psu.edu>
2015-01-20 17:43:10 -0500

    Further improvements courtesy @bgruening and @erasche. Create a base image with build-essential and an ENTRYPOINT that will automatically call the build script.
    Use top-level `build` as before (`./build package`).

--- samtools/build.sh
+++ samtools/build.sh
@@ -1,33 +1,28 @@
 #!/bin/sh
 
-# To be used with:
-#   docker run
-
 pkg=samtools
 version=0.1.19
-build_deps="bzip2 make gcc libc6-dev zlib1g-dev libncurses5-dev"
+build_deps="libc6-dev zlib1g-dev libncurses5-dev"
 urls="
 http://downloads.sourceforge.net/project/samtools/samtools/${version}/samtools-${version}.tar.bz2
 "
 
-DEBIAN_FRONTEND=noninteractive; export DEBIAN_FRONTEND
-
 apt-get -qq update &&
-    apt-get install --no-install-recommends -y wget $build_deps &&
+    apt-get install --no-install-recommends -y $build_deps &&
     mkdir /build &&
     cd /build &&
 
     ( for url in $urls; do
-        wget -q "$url" || false || exit
+        wget "$url" || false || exit
     done ) &&
 
-    tar jxf samtools-0.1.19.tar.bz2 &&
-    cd samtools-0.1.19 &&
+    tar jxf samtools-${version}.tar.bz2 &&
+    cd samtools-${version} &&
     make &&
     mkdir -p /build/dest/bin /build/dest/lib /build/dest/include/bam /build/dest/include/bcf &&
     cp samtools bcftools/bcftools bcftools/vcfutils.pl /build/dest/bin &&
     cp libbam.a bcftools/libbcf.a /build/dest/lib &&
     cp *.h /build/dest/include/bam &&
     cp bcftools/*.h /build/dest/include/bcf &&
-    tar zcf /host/samtools-0.1.19-Linux-x86_64.tar.gz * -C /build/dest
+    tar zcf /host/samtools-${version}-Linux-x86_64.tar.gz * -C /build/dest
 
