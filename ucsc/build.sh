7892ea6d1be2ed7039e713ed89920ae582815eb6
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-02-27 18:01:00 -0600

    Need to copy libs

--- ucsc/build.sh
+++ ucsc/build.sh
@@ -2,7 +2,7 @@
 arch=x86_64
 pkg=ucsc
 version=312
-build_deps="libc6-dev zlib1g-dev libncurses5-dev libmysqlclient-dev unzip libpng-dev"
+build_deps="libc6-dev zlib1g-dev libncurses5-dev libmysqlclient-dev unzip libpng-dev libssl-dev"
 urls="
 http://hgdownload.soe.ucsc.edu/admin/jksrc.v${version}.zip
 "
@@ -16,11 +16,12 @@ apt-get -qq update &&
         wget "$url" || false || exit
     done ) &&
 
-    mkdir -p /bin/${arch}/ /build/dest/bin /build/dest/lib &&
+    mkdir -p $HOME/bin/${arch}/ /build/dest/bin /build/dest/lib &&
     unzip jksrc.v${version}.zip &&
     cd kent/src/lib/ &&
     make &&
     cd ../utils/ &&
     find . -type d -maxdepth 1 -mindepth 1 -exec make -C '{}' \; &&
-    mv /bin/${arch}/* /build/dest/bin/ &&
-    tar zcf /host/ucsc-${version}-Linux-${arch}.tar.gz -C /build/dest .
+    mv $HOME/bin/${arch}/* /build/dest/bin/ &&
+    find $(ldd faToTwoBit | grep -o '=> /[^ ]*' | sed 's/=> //g') -exec cp '{}' /build/dest/lib/ &&
+    tar zcf /host/${pkg}-${version}-Linux-${arch}.tar.gz -C /build/dest .
