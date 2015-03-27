96d90e70cf202ff2a39d55e8c2e361e3b38dc95d
Dave B <dave@bx.psu.edu>
2015-03-27 11:09:22 -0400

    Remove debug output.

--- util/utility_functions.sh
+++ util/utility_functions.sh
@@ -2,16 +2,12 @@
 extract_tarball() {
     tarball=$1
     ext=`echo $tarball | rev | cut -d. -f1 | rev`
-    echo $ext
     case "$ext" in
         gz|tgz)
-            echo "tar zxf $tarball"
             tar zxf $tarball ;;
         bz2|tbz)
-            echo "tar jxf $tarball"
             tar jxf $tarball ;;
         xz)
-            echo "tar Jxf $tarball"
             tar Jxf $tarball ;;
     esac
 }
