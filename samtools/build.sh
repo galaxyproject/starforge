343c155da682e27b8c5add51cea1dd445839528a
Eric Rasche <esr@tamu.edu>
2015-05-26 13:22:29 -0500

    Update samtools to v1.2

--- samtools/build.sh
+++ samtools/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 pkg=samtools
-version=0.1.19
+version=1.2
 build_deps="libc6-dev zlib1g-dev libncurses5-dev"
 urls="
-http://downloads.sourceforge.net/project/samtools/samtools/${version}/samtools-${version}.tar.bz2
+https://github.com/samtools/samtools/releases/download/${version}/samtools-${version}.tar.bz2
 "
 
 apt-get -qq update &&
@@ -19,10 +19,11 @@ apt-get -qq update &&
     tar jxf samtools-${version}.tar.bz2 &&
     cd samtools-${version} &&
     make &&
-    mkdir -p /build/dest/bin /build/dest/lib /build/dest/include/bam /build/dest/include/bcf &&
-    cp samtools bcftools/bcftools bcftools/vcfutils.pl /build/dest/bin &&
-    cp libbam.a bcftools/libbcf.a /build/dest/lib &&
+    mkdir -p /build/dest/bin /build/dest/lib /build/dest/include/bam &&
+    cp samtools /build/dest/bin &&
+    cp libbam.a /build/dest/lib &&
     cp *.h /build/dest/include/bam &&
-    cp bcftools/*.h /build/dest/include/bcf &&
+    cd htslib-1.2.1 &&
+    make bgzip tabix &&
+    cp bgzip tabix /build/dest/bin &&
     tar zcf /host/samtools-${version}-Linux-x86_64.tar.gz -C /build/dest .
-
