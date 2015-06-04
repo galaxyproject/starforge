935029a3761ebd7750da8b5635e8d59a73adff72
Eric Rasche <esr@tamu.edu>
2015-06-04 11:40:48 -0500

    Import build.sh files from master

---
+++ bcftools/build.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+. /util/utility_functions.sh
+
+if [ -z $2 ] ; then
+    pkg=bcftools
+else
+    pkg=$2
+fi
+
+if [ -z $3 ] ; then
+    version=1.0
+else
+    version=$3
+fi
+
+build_deps="libc6-dev zlib1g-dev"
+urls="
+https://github.com/samtools/${pkg}/releases/download/${version}/${pkg}-${version}.tar.bz2
+"
+
+
+apt-get -qq update &&
+    apt-get install --no-install-recommends -y $build_deps &&
+    mkdir /build &&
+    cd /build &&
+
+    ( for url in $urls; do
+        tarball=`download_tarball $url` || false || exit
+    done ) &&
+
+    ( for tarball in * ; do
+      extract_tarball $tarball || false || exit
+    done ) &&
+
+    cd ${pkg}-${version} &&
+    sed -i.bak 's#/usr/local#/build/dest#' Makefile &&
+    make &&
+    make install &&
+    tar zcf /host/${pkg}-${version}-Linux-x86_64.tar.gz -C /build/dest .
+
---
+++ ucsc/build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+arch=x86_64
+pkg=ucsc
+version=312
+build_deps="libc6-dev zlib1g-dev libncurses5-dev libmysqlclient-dev unzip libpng-dev libssl-dev"
+urls="
+http://hgdownload.soe.ucsc.edu/admin/jksrc.v${version}.zip
+"
+
+build=/build
+
+apt-get -qq update &&
+    apt-get install --no-install-recommends -y $build_deps &&
+    mkdir ${build} &&
+    cd ${build} &&
+
+    ( for url in $urls; do
+        wget "$url" || false || exit
+    done ) &&
+
+    mkdir -p $HOME/bin/${arch}/ ${build}/dest/bin ${build}/dest/lib &&
+    unzip jksrc.v${version}.zip &&
+    cd kent/src/lib/ &&
+    make &&
+    cd ../utils/ &&
+    COPT='-O -g -Wl,-rpath,$${ORIGIN}/../lib' &&
+    ORIGIN='$ORIGIN'
+    export COPT ORIGIN &&
+    find . -type d -maxdepth 1 -mindepth 1 -exec make -C '{}' \; &&
+    mv $HOME/bin/${arch}/* ${build}/dest/bin/ &&
+    for lib in $(ldd ${build}/dest/bin/faToTwoBit | grep -o '=> /[^ ]*' | sed 's/=> //g'); do
+        case $lib in
+            */libcrypto.so*|*/libgcc_s.so*|*/libmysqlclient.so*|*/libssl.so*|*/libstdc++.so*)
+                echo "including $lib"
+                cp $lib ${build}/dest/lib/
+                ;;
+        esac
+    done
+    tar zcf /host/${pkg}-${version}-Linux-${arch}.tar.gz -C ${build}/dest .
