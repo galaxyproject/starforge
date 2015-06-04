cf08215b3df58c216c30a56434ede8f14c4dbf29
Eric Rasche <esr@tamu.edu>
2015-06-04 13:00:17 -0500

    Pin to 12.04

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,14 @@
-./build ubuntu atlas
-./build ubuntu bcftools
-./build ubuntu emboos
-./build ubuntu jellyfish
-./build ubuntu kraken
-./build ubuntu ngninx
-./build ubuntu openms
-./build ubuntu pkiss
-./build ubuntu rnashapes
-./build ubuntu samtools
-./build ubuntu tpp
-./build ubuntu ucsc
+./build ubuntu:12.04 atlas
+./build ubuntu:12.04 bcftools
+./build ubuntu:12.04 emboos
+./build ubuntu:12.04 jellyfish
+./build ubuntu:12.04 kraken
+./build ubuntu:12.04 ngninx
+./build ubuntu:12.04 openms
+./build ubuntu:12.04 pkiss
+./build ubuntu:12.04 rnashapes
+./build ubuntu:12.04 samtools
+./build ubuntu:12.04 tpp
+./build ubuntu:12.04 ucsc
 
 python build.py --quiet tbl2asn
