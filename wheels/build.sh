2403814d26a9bd8661912be3c851282f4a44c0b2
Nate Coraor <nate@bx.psu.edu>
2015-07-04 15:12:38 -0400

    Fix issues related to 32 bit docker (running on a 64 bit kernel), generate wheels without the distribution.

--- wheels/build.sh
+++ wheels/build.sh
@@ -3,17 +3,26 @@
 pkgvers=$1
 url=$2
 
+case `getconf LONG_BIT` in
+    32)
+        plat=linux_i686
+        ;;
+    64)
+        plat=linux_x86_64
+        ;;
+esac
+
 mkdir -p /host/$pkgvers &&
 mkdir /build &&
 cd /build &&
 wget --no-check-certificate $url &&
 tar zxf `basename $url` &&
 cd $pkgvers &&
-/python/2.6-ucs2/bin/python setup.py bdist_wheel &&
+/python/2.6-ucs2/bin/python setup.py bdist_wheel --plat-name=${plat} &&
 rm -rf build &&
-/python/2.6-ucs4/bin/python setup.py bdist_wheel &&
+/python/2.6-ucs4/bin/python setup.py bdist_wheel --plat-name=${plat} &&
 rm -rf build &&
-/python/2.7-ucs2/bin/python setup.py bdist_wheel &&
+/python/2.7-ucs2/bin/python setup.py bdist_wheel --plat-name=${plat} &&
 rm -rf build &&
-/python/2.7-ucs4/bin/python setup.py bdist_wheel &&
+/python/2.7-ucs4/bin/python setup.py bdist_wheel --plat-name=${plat} &&
 mv dist/* /host/$pkgvers
