f75c1b0b45fe221c93c43ebb288b414c978d025b
Nate Coraor <nate@bx.psu.edu>
2015-07-05 11:59:49 -0400

    Add some amount of per-package configurability for the wheel builds

--- wheels/build.sh
+++ wheels/build.sh
@@ -18,11 +18,16 @@ cd /build &&
 wget --no-check-certificate $url &&
 tar zxf `basename $url` &&
 cd $pkgvers &&
-/python/2.6-ucs2/bin/python setup.py bdist_wheel --plat-name=${plat} &&
+if [ -f /host/prebuild/$pkgvers ]; then
+    . /host/prebuild/$pkgvers
+else
+    true
+fi &&
+/python/2.6-ucs2/bin/python setup.py $build_args bdist_wheel --plat-name=${plat} &&
 rm -rf build &&
-/python/2.6-ucs4/bin/python setup.py bdist_wheel --plat-name=${plat} &&
+/python/2.6-ucs4/bin/python setup.py $build_args bdist_wheel --plat-name=${plat} &&
 rm -rf build &&
-/python/2.7-ucs2/bin/python setup.py bdist_wheel --plat-name=${plat} &&
+/python/2.7-ucs2/bin/python setup.py $build_args bdist_wheel --plat-name=${plat} &&
 rm -rf build &&
-/python/2.7-ucs4/bin/python setup.py bdist_wheel --plat-name=${plat} &&
+/python/2.7-ucs4/bin/python setup.py $build_args bdist_wheel --plat-name=${plat} &&
 mv dist/* /host/$pkgvers
