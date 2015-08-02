75d7e5a24e66b8866069b058c53057237f4c914e
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-08-02 23:51:12 +0200

    add version 1.2

--- samtools/build.sh
+++ samtools/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 pkg=samtools
-version=0.1.19
-build_deps="libc6-dev zlib1g-dev libncurses5-dev"
+version=1.2
+build_deps="libc6-dev zlib1g-dev libncurses5-dev curl"
 urls="
-http://downloads.sourceforge.net/project/samtools/samtools/${version}/samtools-${version}.tar.bz2
+http://github.com/samtools/samtools/releases/download/${version}/samtools-${version}.tar.bz2
 "
 
 apt-get -qq update &&
@@ -13,16 +13,19 @@ apt-get -qq update &&
     cd /build &&
 
     ( for url in $urls; do
-        wget "$url" || false || exit
+        curl -L -O "$url" || false || exit
     done ) &&
 
     tar jxf samtools-${version}.tar.bz2 &&
     cd samtools-${version} &&
     make &&
-    mkdir -p /build/dest/bin /build/dest/lib /build/dest/include/bam /build/dest/include/bcf &&
-    cp samtools bcftools/bcftools bcftools/vcfutils.pl /build/dest/bin &&
-    cp libbam.a bcftools/libbcf.a /build/dest/lib &&
+    cd htslib-* &&
+    make bgzip tabix &&
+    mkdir -p /build/dest/bin /build/dest/lib /build/dest/include/bam &&
+    cp -f bgzip tabix /build/dest/bin &&
+    cd .. &&
+    cp samtools /build/dest/bin &&
+    cp libbam.a /build/dest/lib &&
     cp *.h /build/dest/include/bam &&
-    cp bcftools/*.h /build/dest/include/bcf &&
     tar zcf /host/samtools-${version}-Linux-x86_64.tar.gz -C /build/dest .
 
