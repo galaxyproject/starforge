02d29fa8ba303e199aeb3f3309068492eb5aa183
Nate Coraor <nate@bx.psu.edu>
2015-08-25 16:40:11 -0400

    Install modified wheel into the "build" python (the one that runs build.py) so that it can use the improved platform detection.

--- wheels/image/update-wheel.sh
+++ wheels/image/update-wheel.sh
@@ -3,7 +3,7 @@
 # quick hack to update the version of my wheel modifications in the wheel images
 #
 
-wheel_version='35e48cf7c4af'
+wheel_version='3f496bdf2708'
 
 images="
 debian6-wheel
@@ -11,26 +11,27 @@ debian7-wheel
 debian8-wheel
 debian9-wheel
 ubuntu12.04-wheel
+ubuntu14.04-wheel
 centos6-wheel
 centos7-wheel
 debian6-wheel-32
 debian7-wheel-32
 debian8-wheel-32
 debian9-wheel-32
+ubuntu12.04-wheel-32
+ubuntu14.04-wheel-32
 centos6-wheel-32
 "
-#ubuntu14.04-wheel
-#ubuntu12.04-wheel-32
-#ubuntu14.04-wheel-32
 
 for image in $images; do
-    echo $image
+    echo "updating ${image}..."
     if [[ $image == *"-32" ]]; then
         linux32='linux32'
     else
         linux32=
     fi
-    docker run --name="${image}-update" natefoo/$image sh -c "mkdir /build && cd /build && curl -O https://bitbucket.org/natefoo/wheel/get/${wheel_version}.tar.gz && tar zxf ${wheel_version}.tar.gz && cd natefoo-wheel-${wheel_version} && $linux32 /python/2.6-ucs2/bin/python setup.py install && rm -rf build && $linux32 /python/2.6-ucs4/bin/python setup.py install && rm -rf build && $linux32 /python/2.7-ucs2/bin/python setup.py install && rm -rf build && $linux32 /python/2.7-ucs4/bin/python setup.py install && cd .. && rm -rf /build"
+    docker run --name="${image}-update" natefoo/$image sh -c "mkdir /build && cd /build && curl -O https://bitbucket.org/natefoo/wheel/get/${wheel_version}.tar.gz && tar zxf ${wheel_version}.tar.gz && cd natefoo-wheel-${wheel_version} && $linux32 python setup.py install && rm -rf build && $linux32 /python/2.6-ucs2/bin/python setup.py install && rm -rf build && $linux32 /python/2.6-ucs4/bin/python setup.py install && rm -rf build && $linux32 /python/2.7-ucs2/bin/python setup.py install && rm -rf build && $linux32 /python/2.7-ucs4/bin/python setup.py install && cd / && rm -rf /build"
     docker commit ${image}-update natefoo/$image
     docker rm -v ${image}-update
+    echo "finished ${image}"
 done
