389f892fcd3903b936226a2e464d26a0df359e7b
Nate Coraor <nate@bx.psu.edu>
2015-08-25 16:38:02 -0400

    Determine what wheels should be built, don't attempt to build them if they've been built, don't continue if they fail to build.

--- wheel.py
+++ wheel.py
@@ -5,7 +5,7 @@ import sys
 import urllib2
 import argparse
 import subprocess
-from os.path import abspath, dirname, join, basename
+from os.path import abspath, dirname, join, basename, exists
 
 import yaml
 
@@ -26,6 +26,8 @@ def main():
 
     assert args.package in wheels['packages'], 'Not in %s: %s' % (WHEELS_YML, args.package)
 
+    version = wheels['packages'][args.package]['version']
+
     if args.image is not None:
         images = [args.image]
     else:
@@ -36,18 +38,50 @@ def main():
             images = wheels['imagesets']['default']
 
     src_cache = join(WHEELS_BUILD_DIR, 'cache')
-    if not os.path.exists(src_cache):
+    if not exists(src_cache):
         os.makedirs(src_cache)
 
     src_url = wheels['packages'][args.package]['src']
     tgz = join(src_cache, basename(src_url))
 
-    if not os.path.exists(tgz):
+    if not exists(tgz):
         with open(tgz, 'w') as handle:
             r = urllib2.urlopen(src_url, None, 15)
             handle.write(r.read())
 
+    plat_cache = join(src_cache, '__platform_cache.json')
+    if not exists(plat_cache):
+        open(plat_cache, 'w').write(yaml.dump({}))
+    platforms = yaml.safe_load(open(plat_cache).read())
+
+    expected = {}
+
     for image in images:
+        plat_name = wheels['images'].get(image, {}).get('plat_name', None)
+        if plat_name is None:
+            if image not in platforms:
+                print 'Caching platform tag for image: %s' % image
+                cmd = [ 'docker', 'run', image, '/python/2.6-ucs2/bin/python',
+                        '-c', 'import wheel.pep425tags; print '
+                        'wheel.pep425tags.get_platforms(major_only=True)[0]' ]
+                platforms[image] = subprocess.check_output(cmd).strip()
+                print 'Platform tag for %s is: %s' % (image, platforms[image])
+                open(plat_cache, 'w').write(yaml.dump(platforms))
+            plat_name = platforms[image]
+        expected[image] = []
+        for py in ('26', '27'):
+            for abi_flags in ('m', 'mu'):
+                whl = '%s-%s-cp%s-cp%s%s-%s.whl' % (args.package, version, py, py, abi_flags, plat_name)
+                expected[image].append(join(WHEELS_DIST_DIR, args.package, whl))
+
+    for image in images:
+        for f in expected[image]:
+            if not exists(f):
+                break
+            print '%s exists...' % f
+        else:
+            print 'Skipping build on %s because all expected wheels exist' % image
+            continue
         try:
             buildpy = wheels['images'][image]['buildpy']
         except:
@@ -58,7 +92,14 @@ def main():
                 image, buildpy, '-u', '/host/build/build.py', args.package, image ]
         print 'Running docker:', ' '.join(cmd)
         subprocess.check_call(cmd)
-
+        missing = []
+        for f in expected[image]:
+            if not exists(f):
+                missing.append(f)
+        if missing:
+            print 'The following expected wheels were not found after the attempted build on %s:' % image
+            print '\n'.join(missing)
+            sys.exit(1)
 
 if __name__ == '__main__':
     main()
