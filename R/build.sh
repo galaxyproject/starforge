40ec04a8a0c218a51821ea168614e655cf6affdb
Bjoern Gruening <bjoern.gruening@gmail.com>
2015-09-05 18:49:02 +0200

    update package description

--- R/build.sh
+++ R/build.sh
@@ -9,17 +9,17 @@ else
 fi
 
 if [ -z $3 ] ; then
-    version=3.2.0
+    version=3.2.1
 else
     version=$3
 fi
 
-build_deps="libc6-dev zlib1g-dev tcl-dev tk-dev libblas-dev liblapack-dev libcairo2-dev libpng12-dev libtiff4-dev libjpeg-dev libreadline-dev default-jre"
+build_deps="libc6-dev zlib1g-dev tcl-dev tk-dev libblas-dev liblapack-dev libcairo2-dev libpng12-dev libtiff4-dev libjpeg-dev libreadline-dev"
 urls="http://cran.rstudio.com/src/base/R-3/$pkg-$version.tar.gz"
 
 apt-get -qq update &&
     apt-get install --no-install-recommends -y $build_deps &&
-    mkdir /build &&
+    mkdir -p /build/dest/lib/ &&
     cd /build &&
 
     ( for url in $urls; do
@@ -34,8 +34,6 @@ apt-get -qq update &&
     export LDFLAGS='-Wl,-rpath,\$$ORIGIN/../lib' &&
     ./configure --with-readline=yes \
                 --with-x=no \
-                --with-blas \
-                --with-lapack \
                 --with-cairo \
                 --with-libpng \
                 --enable-R-shlib \
@@ -49,7 +47,11 @@ apt-get -qq update &&
     sed -i 's#/build/dest#$(R_ROOT_DIR)#g' /build/dest/lib/R/etc/Makeconf &&
     cp /usr/lib/libgfortran* /build/dest/lib/ &&
     cp /usr/lib/libgomp* /build/dest/lib/ &&
-    cp /usr/lib/libblas.so.3gf /build/dest/lib &&
+    cp /usr/lib/libblas* /build/dest/lib -R &&
+    cp /usr/lib/libcairo* /build/dest/lib &&
+    cp /usr/lib/libreadline* /build/dest/lib &&
+    cp /usr/lib/liblapack* /build/dest/lib &&
+    cp /usr/lib/libpng* /build/dest/lib &&
     cd /build/dest/lib &&
     ln -s libgfortran.so.3 libgfortran.so &&
     ln -s libgomp.so.1.0.0 libgomp.so &&
