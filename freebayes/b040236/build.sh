f108e46e26c7cf36e617151cc41b500f2a54dfd0
Bjoern Gruening <bjoern.gruening@gmail.com>
2016-02-01 13:47:57 +0100

    add older freebayes packages

---
+++ freebayes/b040236/build.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+. /util/utility_functions.sh
+
+pkg="freebayes"
+build_deps="libc6-dev git-core cmake zlib1g-dev"
+url="
+https://github.com/ekg/freebayes.git
+"
+revision="b040236"
+
+binaries="
+freebayes
+bamleftalign
+"
+
+apt-get -qq update &&
+    apt-get install --no-install-recommends -y $build_deps &&
+    mkdir /build &&
+    cd /build &&
+
+    git clone --recursive $url
+    cd freebayes
+    git checkout $revision
+
+    mkdir -p /build/dest/bin &&
+    make &&
+    ( for binary in $binaries ; do
+        cp ./bin/$binary /build/dest/bin
+    done ) &&
+    tar zcf /host/${pkg}-${revision}-Linux-x86_64.tar.gz -C /build/dest .
+
